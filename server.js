4f5c7329b4ba9cd6625087515f46ee93843c11ef
Aliakbar <aispolayev@gmail.com>
2022-05-16 23:28:07 +0600

    h3

--- server.js
+++ server.js
@@ -1,46 +1,46 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+let port = process.env.PORT || 3000;
 const mongoose = require('mongoose');
 
 
-var isWeekend = require('is-weekend')();
+// var isWeekend = require('is-weekend')();
 
-console.log(isWeekend); // True or False;
+// console.log(isWeekend); // True or False;
 app.use(express.static(__dirname + '/image'));
 app.use(express.static(__dirname + '/css'));
 app.get('/', ((req, res) => {
     res.sendFile(__dirname + '/index.html')
-}))
+}));
 
 app.get('/intel', ((req, res) => {
     res.sendFile(__dirname + '/intel.html')
-}))
+}));
 
 app.get('/filter', ((req, res) => {
     res.sendFile(__dirname + '/filter_page.html')
-}))
+}));
 
 app.get('/nvidia', ((req, res) => {
     res.sendFile(__dirname + '/nvidia.html')
-}))
+}));
 app.get('/contacts', ((req, res) => {
     res.sendFile(__dirname + '/contacts.html')
-}))
+}));
 app.get('/amd', ((req, res) => {
     res.sendFile(__dirname + '/amd.html')
-}))
+}));
 
 async function start(){
     try{
-        await mongoose.connect('mongodb+srv://aliakbar:Buzukbuzuk2003@cluster0.m9pvz.mongodb.net/shop')
+        await mongoose.connect('mongodb+srv://aliakbar:Buzukbuzuk2003@cluster0.m9pvz.mongodb.net/?retryWrites=true&w=majority')
         app.listen(port, () => {
             console.log(`App listening at http://localhost:${port}`)
         })
     } catch (e){
         console.log(e)
     }
-}
+};
 
-start()
+start();
 
